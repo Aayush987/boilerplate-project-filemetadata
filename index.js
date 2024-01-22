91c7c482a41b365929850982c3918f95fb57cdd1
Aayush <aayushsharma47163@gmail.com>
2024-01-22 12:55:24 +0530

    Added a scheduler to ping server

--- index.js
+++ index.js
@@ -3,11 +3,26 @@ var cors = require('cors');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 require('dotenv').config()
+var cron = require('node-cron');
+var axios = require('axios');
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+cron.schedule('*/10 * * * *', () => {
+  axios.get('https://file-metadata-a1d1.onrender.com/')
+     .then(resonse => {
+       console.log('Server Pinged successfully');
+     })
+     .catch(error => {
+       console.log(error);
+     });
+})
+
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
